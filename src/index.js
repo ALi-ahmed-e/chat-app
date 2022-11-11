2f6345cfc0b48e7bb2d98759283b8e5718c04f65
Ali ahmed <aa5325368@gmail.com>
2022-11-11 16:34:48 +0200

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
-import Chatreducer from './store/reducers/chatreducer';
 import store from './store';
 import {Provider} from 'react-redux'
 
